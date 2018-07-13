d47e33763c835bcc4c04f2439e88d1d3bad30e97
dkanmaz <deniz.kanmaz@nesine.com>
2018-07-13 14:31:26 +0300

    Added copyright notification to each source file.

--- MainSerial.h
+++ MainSerial.h
@@ -3,15 +3,15 @@
 //  LICENCE V3. Use of this source code is governed by a license
 //  that can be found in the LICENSE file.
 
-//	This program is distributed in the hope that it will be useful,
-//	but WITHOUT ANY WARRANTY; without even the implied warranty of
-//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-//	LICENSE file for more details.
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  LICENSE file for more details.
 
-//	You should have received a copy of the LICENSE file along with
-//	with this program. If not, see <http://www.gnu.org/licenses/>.
+//  You should have received a copy of the LICENSE file along with
+//  with this program. If not, see <http://www.gnu.org/licenses/>.
 
-//	E-mail: denizkanmaz@gmail.com
+//  E-mail: denizkanmaz@gmail.com
 
 class MainSerial
 {
